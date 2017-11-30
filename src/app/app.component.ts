f307176af9632b749ed468397a9c25d097209cb8
Hari <hari02212@gmail.com>
2017-11-30 18:31:28 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public imageSources: string[] = [
+    'assets/img/1.jpg',
+    'assets/img/2.jpg',
+    'assets/img/3.jpg'
+  ];
+
+  public config: ICarouselConfig = {
+    verifyBeforeLoad: true,
+    log: false,
+    animation: true,
+    animationType: AnimationConfig.SLIDE,
+    autoplay: true,
+    autoplayDelay: 3000,
+    stopAutoplayMinWidth: 768
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'angular4-carousel';
+import {Ng2PageScrollModule} from 'ng2-page-scroll';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AboutComponent,
+    PricingComponent,
+    TeamComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule,
+    Ng2PageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
